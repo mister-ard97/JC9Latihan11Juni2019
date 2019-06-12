e567176f81f356d35089360399af04ae2520f8e8
Muhammad Reza Ardiansyah <48369705+mister-ard97@users.noreply.github.com>
2019-06-12 09:26:25 +0700

    Update latihanLebaran3.js

--- latihanLebaran3.js
+++ latihanLebaran3.js
@@ -1,8 +1,8 @@
 var domainName = function (param) {
-    param = param.replace('http://', '').replace('www.', '').replace('.com', '').split('/');
+    param = param.replace('http://', '').replace('https://', '').replace('www.', '').replace('.com', '').split('/');
     return param[0];
 }
 
 console.log(domainName('http://github.com/carbonfive/raygun'));
 console.log(domainName('http://www.zombie-bites.com'));
-console.log(domainName('http://www.cnet.com'));
+console.log(domainName('https://www.cnet.com'));
